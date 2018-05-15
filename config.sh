c9b6b5361797a19b35ae580b77fe831ccbddf1cf
Shawn Gibbs <shawngib@microsoft.com>
2018-05-15 15:03:11 -0400

    Bash scripts

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # install dotnet core
 sudo wget -q packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+sudo DEBIAN_FRONTEND=noninteractive dpkg -i packages-microsoft-prod.deb
 sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.1.200
@@ -15,9 +15,9 @@ git clone https://github.com/gibbsa/sampleproject.git
 sudo apt-get install -y nginx
 sudo service nginx start
 sudo touch /etc/nginx/sites-available/default
-sudo wget https://raw.githubusercontent.com/gibbsa/sampleproject/default -O /etc/nginx/sites-available/default
+sudo wget https://raw.githubusercontent.com/gibbsa/sampleproject/master/default -O /etc/nginx/sites-available/default
 #sudo cp /opt/music/nginx-config/default /etc/nginx/sites-available/
-#sudo nginx -s reload
+sudo nginx -s reload
 
 # update and secure music config file
 #sed -i "s/<replaceserver>/$1/g" /opt/music/config.json
