c9d95bba1abcb88f6ca65d4b8c3c3f98bafebc42
Shawn Gibbs <shawngib@microsoft.com>
2018-05-15 15:41:41 -0400

    fixed bash

--- config.sh
+++ config.sh
@@ -18,22 +18,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install -y nginx
 sudo service nginx start
 sudo touch /etc/nginx/sites-available/default
 sudo wget https://raw.githubusercontent.com/gibbsa/sampleproject/master/default -O /etc/nginx/sites-available/default
-#sudo cp /opt/music/nginx-config/default /etc/nginx/sites-available/
 sudo nginx -s reload
 
-# update and secure music config file
-#sed -i "s/<replaceserver>/$1/g" /opt/music/config.json
-#sed -i "s/<replaceuser>/$2/g" /opt/music/config.json
-#sed -i "s/<replacepass>/$3/g" /opt/music/config.json
-#sudo chown $2 /opt/music/config.json
-#sudo chmod 0400 /opt/music/config.json
-
-# config supervisor
-#sudo apt-get install -y supervisor
-#sudo touch /etc/supervisor/conf.d/music.conf
-#sudo wget https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/supervisor/music.conf -O /etc/supervisor/conf.d/music.conf
-#sudo service supervisor stop
-#sudo service supervisor start
-
-# pre-create music store database
-#/usr/bin/dotnet /opt/music/MusicStore.dll &
+# run app
+/usr/bin/dotnet ./Sample App/bin/Debug/netcoreapp2.0/samplecoreapp.dll &
