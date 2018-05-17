489308fe363b675e82a4af831dff1d4086654244
gibbsa <39273120+gibbsa@users.noreply.github.com>
2018-05-17 10:44:55 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,8 +10,12 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes dotnet-sdk-2.1.200
 
 # download application
-cd ~/
+cd /home/shawngib
+sudo rm -rf sampleproject
 git clone https://github.com/gibbsa/sampleproject.git
+cd sampleproject/
+dotnet publish
+sudo cp -a /home/shawngib/sampleproject/bin/Debug/netcoreapp2.0/publish /var/sampleproject
 
 # install nginx, update config file
 sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install -y nginx
@@ -20,5 +24,12 @@ sudo touch /etc/nginx/sites-available/default
 sudo wget https://raw.githubusercontent.com/gibbsa/sampleproject/master/default -O /etc/nginx/sites-available/default
 sudo nginx -s reload
 
+sudo apt-get install supervisor
+#sudo wget https://raw.githubusercontent.com/gibbsa/sampleproject/master/sampleproject.conf
+sudo cp /home/shawngib/sampleproject/sampleproject.conf /etc/supervisor/conf.d/sampleproject.conf
+sudo service supervisor stop 
+sudo service supervisor start
+# sudo pico /etc/supervisor/conf.d/sampleproject.conf
+
 # run app
-/usr/bin/dotnet "./Sample App/bin/Debug/netcoreapp2.0/samplecoreapp.dll" &
+# /usr/bin/dotnet "./Sample App/bin/Debug/netcoreapp2.0/samplecoreapp.dll" &
